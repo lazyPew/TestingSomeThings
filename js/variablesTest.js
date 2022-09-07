1a862fa88a9e1b6061e61b5e9bcd686fdf772e64
lazyPew <sailar276@gmail.com>
2022-09-07 09:52:22 +0300

    JS: trying to work with JavaScript

---
+++ js/variablesTest.js
@@ -0,0 +1,16 @@
+// "use strict"
+// let admin;
+// let name;
+
+// name = "John";
+// admin = name;
+// console.log(admin);
+'use strict';
+
+let value = prompt('Какое "официальное" название JavaScript?', '');
+
+if (value == 'ECMAScript') {
+    alert('Верно!');
+} else {
+    alert('Не знаете? ECMAScript!');
+}
